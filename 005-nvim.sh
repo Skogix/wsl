385af72eacd039e25a508704c13b3510e5d15248
Emil Skogsund <skogix@gmail.com>
2022-09-22 15:06:50 +0200

    before rework

--- 005-nvim.sh
+++ 005-nvim.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#sudo pacman -S neovim fd python-pip nodejs npm perl ruby
-#yay -S ruby-neovim nodejs-neovim python-pynvim python2-pynvim
+sudo pacman -S neovim fd python-pip nodejs npm perl ruby
+yay -S ruby-neovim nodejs-neovim python-pynvim python2-pynvim
 ## download plug
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 ## python
-#/usr/bin/python3 -m pip install pynvim
+/usr/bin/python3 -m pip install pynvim
 ## ruby
-#export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
-#export PATH="$PATH:$GEM_HOME/bin"
-#gem install neovim
+export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+export PATH="$PATH:$GEM_HOME/bin"
+gem install neovim
 ## nodejs
-#sudo npm install -g neovim
-#sudo npm install -g yarn
+sudo npm install -g neovim
+sudo npm install -g yarn
 #nvim +PlugInstall +qall
 #nvim +UpdateRemotePlugins +qall
 
 
 
 
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-rm -rf ~/.cache/nvimrm -rf $HOME/.config/nvim
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+#rm -rf ~/.config/nvim
+#rm -rf ~/.local/share/nvim
+#rm -rf ~/.cache/nvimrm -rf $HOME/.config/nvim
+#git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
---
+++ dotfiles/plain/common/.config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,15 @@
+vim.o.relativenumber = true
+vim.g['python3_host_prog'] = '/usr/sbin/python3'
+local M = {}
+
+M.ui = {
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark",
+}
+
+M.plugins = require "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
